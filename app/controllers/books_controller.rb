e5f6039c1748a82ca7452726166da7cdfdbcb9b5
yuki-yamada-man <major.in.from@i.softbank.jp>
2021-07-15 11:06:10 +0000

    5th

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,21 +5,41 @@ class BooksController < ApplicationController
   end
 
   def create
+    @book = Book.new(book_params)
+    book.save
+    redirect_to book_path
   end
 
   def index
+    @book = Book.all
   end
 
   def show
+    @book = Book.find(params[id])
   end
 
   def edit
+    @book = Book.find(params[id])
   end
 
   def update
+    @book = Book.find(params[id])
+    book.update
+    redirect_to book_path
   end
 
   def destroy
+    @book =Book.find(params[id])
+    book.destroy
+    redirect_to book_path
   end
+  
+  
+  preivate
+  
+  def book_params
+    params.require(:book).permit(:title,:body)
+    e
+    
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,11 @@ class UsersController < ApplicationController
 
   def update
   end
+  
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image_id)
+  end
 end
